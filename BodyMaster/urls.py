502dddeb03e366913c396cbdb08e4916fb2e3567
NachoCodeX <nachocodex@gmail.com>
2017-07-12 13:34:46 -0500

    heroku3

--- BodyMaster/urls.py
+++ BodyMaster/urls.py
@@ -7,6 +7,3 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^',include('apps.accounts.urls',namespace="accounts")),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
